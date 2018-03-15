164baf69ea50973af7b1261ce362a884885e05b2
Jesse Chan <jc@lineageos.org>
2018-03-15 12:45:45 -0700

    Initial commit
    Signed-off-by: Jesse Chan <jc@lineageos.org>

---
+++ AndroidProducts.mk
@@ -0,0 +1,1 @@
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_star2lte.mk
---
+++ omni_star2lte.mk
@@ -0,0 +1,11 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier
+PRODUCT_DEVICE := star2lte
+PRODUCT_NAME := omni_star2lte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
